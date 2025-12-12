d68fb295a67bf56d9f87280afc3363f8a09f137b
yvan-hirwa <hirwakaranganwayvan@gmail.com>
2025-12-12 11:32:56 +0200

    Q2 Mental model

---
+++ Mentalmodel/Q02.ts
@@ -0,0 +1,10 @@
+// Debug 
+// const scores: number[] = [95, "100", 87];
+// scores.push(82);
+// console.log(scores.reduce((a, b) => a + b));
+
+
+
+const scores: number[] = [95, 100, 87];
+scores.push(82);
+console.log(scores.reduce((a, b) => a + b));
