3b1830ead71cc94f1e3ad339d962220610d12a04
yvan-hirwa <hirwakaranganwayvan@gmail.com>
2025-12-16 14:58:17 +0200

    unknown type in TS

---
+++ unknownTypes/mysterybox.ts
@@ -0,0 +1,23 @@
+// Challenge: The "Mystery Box"
+// I want you to write a function that safely handles a "mystery input".
+
+// Create a function called printLength.
+
+// It takes one argument: input (type unknown).
+
+// Inside the function:
+
+// If the input is a string, log its length.
+
+// If the input is an array, log the number of items (length).
+
+// If it is anything else, log "Cannot measure length".
+
+// (Hint: You will need typeof for the string, and Array.isArray() for the array).
+
+function printLength(input: unknown){
+    if(typeof input === 'string' || Array.isArray(input)) return input.length
+    else return 'Cannot measure length'
+}
+
+// console.log(printLength('hello'))   //TEST
